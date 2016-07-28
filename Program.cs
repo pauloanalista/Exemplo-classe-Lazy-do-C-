f08e1b7e8d6051ccaba17dc84e3ca00e7a70a218
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2016-07-28 11:43:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criei uma instancia da categoria
+            Categoria c = new Categoria();
+
+            //Setei um valor para a propriedade NomeCategoria, mas não setei nada para o usuário
+            c.NomeCategoria = "Transporte";
+
+            //Imprimo o valor da propriedade NomeCategoria
+            Console.WriteLine("Obtem nome da instancia da Categoria = " + c.NomeCategoria);
+
+            //Verifico se a propriedade UsuarioCadastro foi instanciada através do método IsValueCreated
+            Console.WriteLine("UsuarioCadastroInstanciado = " + c.UsuarioCadastro.IsValueCreated);
+
+            //Invoco a propriedade do usuário através do Value
+            Console.WriteLine("Resgato o valor da propriedade do usuario");
+            Console.WriteLine("Obtem nome da instancia do UsuarioCadastro = " + c.UsuarioCadastro.Value.Nome);
+
+            //Verifico se a propriedade UsuarioCadastro foi instanciada através do método IsValueCreated
+            Console.WriteLine("UsuarioCadastroInstanciado = " + c.UsuarioCadastro.IsValueCreated);
+
+            //Fecho o prompt após pressionar uma tecla
+            Console.ReadKey();
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Texto");
+
+        }
+
+        public class Categoria
+        {
+            public Lazy<Usuario> UsuarioCadastro { get; set; }
+
+            public string NomeCategoria { get; set; }
+
+            public Categoria()
+            {
+                this.UsuarioCadastro = new Lazy<Usuario>();
+            }
+        }
+
+        public class Usuario
+        {
+            public Usuario()
+            {
+                this.Nome = "Paulo Rogério";
+            }
+            public string Nome { get; set; }
+        }
+    }
+}
